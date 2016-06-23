ab11c6041e5c82d79b2a7b7e8e66b13fc76f8097
Long Tran <artsandcrass@gmail.com>
2016-06-23 13:04:39 -0700

    initial commit

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,48 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+p "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+p "zom".insert(1, 'o')
 # => “zoom”
 
-# "enhance".<???>
+p "enhance".center(18)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+p "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+p "the usual".concat(" suspects")
+p "the usual".insert(9," suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+p " suspects".prepend("the usual")
+p " suspects".insert(0,"the usual")
+p "suspects".replace("the usual suspects")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+p "The case of the disappearing last letter".chomp('r')
+p "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+p "The mystery of the missing first letter".delete('T')
+p "The mystery of the missing first letter".delete "t".capitalize
+p "The mystery of the missing first letter".delete "t".upcase
+p "The mystery of the missing first letter".delete "The mystery of the missing first letter"[0]
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+p "Elementary,    my   dear        Watson!".scan(/[^\s]{2,}/).join(" ")
+p "Elementary,    my   dear        Watson!".squeeze("  ")
+
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
-# => 122 
+p 'z'.ord
+# => 122
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+# 122 is the Decimal value for z in an Ascii chart. Since computers can only understand numbers,
+# an Ascii chart allows us to see bits values of letters and characters.
+
+p "How many times does the letter 'a' appear in this string?".scan(/(a)/).length
 # => 4
