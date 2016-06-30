5d0df3bb4fcbead998ac37e93eddd6daac32176c
Long Tran <artsandcrass@gmail.com>
2016-06-29 23:54:34 -0700

    add puppy attributes and behaviors

---
+++ ruby/puppy.rb
@@ -0,0 +1,26 @@
+# Assignment 6.1 Intro to Clasess
+
+
+# How might you design a Puppy class? What sorts of characteristics and
+# behavior might a Puppy class involve? Using a format similar to the one we used
+# for Eyelashus Adorableus, come up with some characteristics and behavior for a
+# Puppy class.
+
+
+
+
+# designing a Puppy class
+
+class Puppy
+end
+
+
+# some characteristics or attributes would be:
+# dog type
+# age
+# name
+
+# instance methods or behaviors for the class include:
+# a bark method
+# a fetch method
+# a cuddle method
