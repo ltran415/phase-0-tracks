a15f3e95f6d55091ff93387fc479d1fd5b6bd098
Long Tran <artsandcrass@gmail.com>
2016-07-01 13:51:16 -0700

    write a loop program to create 1000 santas

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,8 @@
 class Santa
 
+  attr_reader :reindeer_ranking
+  attr_accessor :name
+
   def initialize(name, gender, ethnicity, hair_color)
     @name = name
     @gender = gender
@@ -7,7 +10,7 @@ class Santa
     @hair_color = hair_color
     puts "\n"
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-    @age = 0
+    @age = rand(0..140)
     puts "Initializing Santa instance ..."
   end
 
@@ -21,29 +24,32 @@ class Santa
 
   def about
     puts "name: #{@name}"
+    puts "age: #{@age}"
     puts "gender: #{@gender}"
     puts "ethnicity: #{@ethnicity}"
     puts "hair color: #{@hair_color}"
   end
 
-#getter methods
-
-  def reindeer_ranking
-    @reindeer_ranking
-  end
-
-  def name
-    @name
-  end
-
-
-#setter methods
-
-  def name=(name)
-    @name = name
-  end
-
+# #getter methods
+#
+#   def reindeer_ranking
+#     @reindeer_ranking
+#   end
+#
+#   def name
+#     @name
+#   end
+#
+#
+# #setter methods
+#
+#   def name=(name)
+#     @name = name
+#   end
+#
 end
+
+# ------------------------------------------------
 
 # driver code
 
@@ -52,10 +58,26 @@ santa.speak
 santa.eat_milk_and_cookies("chocolate chip")
 santa.name = "Sonic the mole"
 puts "These are #{santa.name}'s favorite reindeers starting with: #{santa.reindeer_ranking}"
-#santa.speak = "Now off to deliver presents and stuff!"
 
 
+# ------------------------------------------------
+# pseudocode
 
+#input: a bunch of new santa instances
+#steps:
+  # create an empty array
+  # create example arrays with initialize information to iterate through
+  # use objects in arrays as arguments for the state of our instance
+#output: many instances with initialize information
+
+# Use our array of example genders and an array of example ethnicities (and feel free to add to it if you like
+# -- each array could have a lot more options in it!) to create your Santas with a randomly selected gender
+# and a randomly selected ethnicity. (How do you randomly select an array item? The Array documentation should be able to help you out there!)
+# Set your new Santa's age to a random number between 0 and 140.
+# No need to store your Santas in a data structure, but your program should print out the attributes of
+# each Santa using the instance methods that give you access to that data.
+
+# ------------------------------------------------
 # ultilizing arrays to create instances
 
 santas = []
@@ -63,7 +85,16 @@ example_names = ["Luffy", "Zoro", "Nami", "Robin", "Chopper", "Brooks", "Sanji"]
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 example_hair_colors = ["Blue", "Silver", "Red", "Pink", "Rainbow", "Green", "Lavender"]
+#example_age = rand(0..140)
 
-example_genders.length.times do |i|
-  santas << Santa.new(example_names[i], example_genders[i], example_ethnicities[i], example_hair_colors[i]).about
+# using a loop to create many santas
+index = 0
+
+while index < 1000
+
+  example_genders.length.times do |i|
+    santas << Santa.new(example_names.sample, example_genders.sample, example_ethnicities.sample, example_hair_colors.sample).about
+  end
+  santas.each {|age| age = @age }
+  index += 1
 end
