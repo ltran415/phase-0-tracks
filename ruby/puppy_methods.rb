eb280f915f0dbc3314591d09eac3d904834a6fb4
Long Tran <artsandcrass@gmail.com>
2016-07-01 23:01:30 -0700

    add new instance methods and driver code

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,37 @@ class Puppy
     toy
   end
 
+  def speak(integer)
+    integer.times{ puts "woof!" }
+    return nil
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+
+    if human_years <= 2
+      human_years *= 5
+    else
+      human_years *= 4
+    end
+  end
+
+  def play_dead
+    puts "*plays dead*"
+  end
+
+
 end
 
+fido = Puppy.new
+
+puts fido.fetch("ball")
+
+puts fido.speak(4)
+
+puts fido.dog_years(18)
+
+puts fido.play_dead
