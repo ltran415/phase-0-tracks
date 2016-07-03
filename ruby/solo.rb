84cf5cb9c51a9b6fbdd6a235e2f8988d5a053225
Long Tran <artsandcrass@gmail.com>
2016-07-03 16:14:01 -0700

    cleaned eroneous code

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -163,38 +163,3 @@ user_tree = nil
       my_trees.each {|i| puts "#{tree.tree_types}. The #{i.common_tree_name} trees are found in the #{i.tree_region}."}
     end
   end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Design (as opposed to actually building) a class representing anything you like.
-#  (Please make it something we have at least heard of.) Whether you want to build
-#   a fire-breathing dragon, a dress, or a soccer ball, we hereby grant you the freedom
-#   to implement it. No matter what you design, your class must have:
-#
-# at least three attributes (using at least two data types) whose values will vary
-# from instance to instance (example: age)
-# at least three methods, at least one of which takes an argument
-# You're welcome to creative about these: a dress can have a swish method,
-# a soccer ball can have an inflatedness attribute, and ... we hope it's obvious
-# what a fire-breathing dragon can do, but if not, you'll figure it out.
