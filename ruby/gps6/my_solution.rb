65e7c8a4af125e160037563913309d238561fea2
Long Tran <artsandcrass@gmail.com>
2016-07-14 15:33:14 -0700

    created reports for all states and made code DRY

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,7 +1,7 @@
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# I worked on this challenge [by myself, with: n/a ].
+# I spent [1.5] hours on this challenge.
 
 # EXPLANATION OF require_relative
 
