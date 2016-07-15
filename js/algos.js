fb84b5fbf719162f1c0cb44b1f22a5f3342a7c53
Long Tran <artsandcrass@gmail.com>
2016-07-14 23:00:40 -0700

    add function match_key_word and random_word_gen, add driver code to test

--- js/algos.js
+++ js/algos.js
@@ -6,12 +6,12 @@
   // create a varible for the longest string and set it to nothing
   // compare the longest string varible with the array object's length
   // if the length is greater than the longest string varible then add it, otherwise skip
-  // return the longest string varible 
+  // return the longest string varible
 
 function longest_char(array){
   var longest = "";
 
-  for(var i =0; i < array.length; i++){
+  for (var i = 0; i < array.length; i++){
     if (longest.length < array[i].length){
       longest = array[i];
     }
@@ -20,6 +20,69 @@ function longest_char(array){
   return longest;
 }
 
+// input: two objects
+// output: a boolean
+// steps:
+  // create a function with two objects as parameters
+  // compare key-value pairs to eachother
+  // if any key-value pair match return true
+  // if no key-value pairs match return false
+
+function match_key_value(obj1, obj2) {
+
+  for (var key in obj1){
+    if (obj1[key] == obj2[key]){
+      return true;
+    }
+  }
+  return false;
+}
+
+// input: a integer for length
+// output: an array of strings of the given length
+// steps:
+  // create a function with an integer as a parameter
+  // create a new array
+  // create an alphabet
+  // create random words based on the integer given
+  // return new array with added words
+
+function random_word_gen(integer) {
+
+  var words = [];
+
+  for (var i = 0; i < integer; i++){
+
+    var alphabet = ["a","b","c","d","e","f","g","h","i","j",
+                    "k","l","m","n","o","p","q","r","s","t",
+                    ,"v","w","x","y","z"];
+                    
+    var random_num = Math.floor((Math.random() * 10) + 1);
+    var random_num2 = Math.floor((Math.random() * 10) + 1) + random_num;
+
+    my_strings = alphabet.slice(random_num, random_num2).join("")
+    words.push(my_strings);
+
+  };
+
+  return words;
+
+}
+
+
+
+// ---------------------------------------------------
+
+// Driver code: Find the Longest Phrase
+
 console.log(longest_char(["I","Love","You"]))
 console.log(longest_char(["Hello","How","Are","You"]))
 console.log(longest_char(["Get","Down","Tonight"]))
+
+// Driver code: Find a Key-Value Match
+
+console.log(match_key_value({name: "Steven", age: 54}, {name: "Tamir", age: 54}))
+console.log(match_key_value({nam: "Buddy", age: 34}, {name: "Tamir", age: 54}))
+console.log(match_key_value({nam: "Buddy", age: 54}, {name: "Tamir", age: 54}))
+
+console.log(random_word_gen(4))
