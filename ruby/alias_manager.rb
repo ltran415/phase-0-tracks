999304b280a67c28fa491e821a55d19684a02c1d
Long Tran <artsandcrass@gmail.com>
2016-07-25 16:40:25 -0700

    Add method to take a vowel letter and move it to the next vowel letter

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -15,15 +15,45 @@ def alias(string_name)
   return swap_names
 end
 
-# def next_vowel(char)
-#   vowels = 'aeiou'
-#   vowels.split("").each do |i|
-#
-#     if char == i
-#
-#
-#     end
-#
+# takes a string and moves the each vowel to the next vowel in 'aeiou'
+#input: string
+#output: all the vowels in the string moved to the next letter in the 'aeiou'
+def next_vowel(string)
+  vowels = ['a','e','i','o','u']
+  new_string = ""
+  split_letters = string.split("")
+  #=> ["L", "o", "n", "g"]
+  split_letters.each do |i|
+   if i == vowels[0]
+     i = vowels[1]
+   elsif i == vowels[1]
+     i = vowels[2]
+   elsif i == vowels[2]
+     i = vowels[3]
+   elsif i == vowels[3]
+     i = vowels[4]
+   elsif i == vowels[4]
+      i = vowels[0]
+   end
+   new_string << i
+  end
+   new_string
+end
+
+p next_vowel('Long Tran')
+
+
+
+
+# vowels = ['a','e','i','o','u']
+# if vowels.index(char) == 0
+#   char = 'e'
+# elsif vowels.index(char) == 1
+#   char = 'i'
+# elsif vowels.index(char) == 2
+#   char = 'o'
+# elsif vowels.index(char) == 3
+#   char = 'o'
+# elsif vowels.index(char) == 4
+#   char = 'a'
 # end
-#
-# p next_vowel('a')
