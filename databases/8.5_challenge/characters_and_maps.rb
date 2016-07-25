61430354f279227a49b8e9f7d6c93ada7f184e2c
Long Tran <artsandcrass@gmail.com>
2016-07-25 01:10:25 -0700

    Created module and methods for heroes data" ;

---
+++ databases/8.5_challenge/characters_and_maps.rb
@@ -0,0 +1,75 @@
+
+module CharacterData
+
+  def characters
+    [
+      "The Butcher",
+      "Li-Ming",
+      "Valla",
+      "Azmodan",
+      "Nazeebo",
+      "Xul",
+      "Khara,zim",
+      "Diablo",
+      "Johanna",
+      "Leoric",
+      "Sonya",
+      "Tyrael",
+      "Kerrigan",
+      "Nova",
+      "Raynor",
+      "Tychus",
+      "Zeratul",
+      "Abathur",
+      "Sgt. Hammer",
+      "Zagara",
+      "Lt. Morales",
+      "Tassadar",
+      "Artanis",
+      "Dehaka",
+      "Chromie",
+      "Falstad",
+      "Gall (Cho'gall)",
+      "Gul'dan",
+      "Greymane",
+      "Illidan",
+      "Jaina",
+      "Kael'thas",
+      "Lunara",
+      "Thrall",
+      "Gazlowe",
+      "Medivh",
+      "Murky",
+      "Sylvanas",
+      "Brightwing",
+      "Li Li",
+      "Malfurion",
+      "Rehgar",
+      "Tyrande",
+      "Uther",
+      "Anub'arak",
+      "Arthas",
+      "Chen",
+      "Cho (Cho'gall)",
+      "Elite Tauren Chieftain",
+      "Muradin",
+      "Rexxar",
+      "Stitches",
+    ]
+  end
+
+  def maps
+    [
+      "Tomb of the Spider Queen",
+      "Sky Temple",
+      "Towers of Doom",
+      "Blackheart's Bay",
+      "Cursed Hollows",
+      "Infernal Shrines",
+      "Battlefeild of Eternity",
+      "Garden of Terror",
+      "Dragon Shire",
+      "Haunted Mines"
+    ]
+  end
+end
