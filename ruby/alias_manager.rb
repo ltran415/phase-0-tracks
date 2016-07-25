b94daab65a2f686d53e1072d0c8597a954e4e581
Long Tran <artsandcrass@gmail.com>
2016-07-25 14:07:13 -0700

    Add method that swaps the first and last name

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -7,3 +7,23 @@
     # Split the first and last name and swap the position
     # Change the vowels of the name to the next vowel in "a,e,i,o,u" and change
     # the consonants to the next consonants in the alphabet.
+
+def alias(string_name)
+  split_names = string.split_string
+  #=> ['Long','Tran']
+  swap_names = split_names.shuffle
+  return swap_names
+end
+
+# def next_vowel(char)
+#   vowels = 'aeiou'
+#   vowels.split("").each do |i|
+#
+#     if char == i
+#
+#
+#     end
+#
+# end
+#
+# p next_vowel('a')
