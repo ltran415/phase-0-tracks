18e7ada1ec4623f1c759b3efa2bd4aa2cf989aa8
Long Tran <artsandcrass@gmail.com>
2016-07-24 19:23:57 -0700

    Create Sql database, create a table with map name, player_1, player_2

--- databases/8.5_challenge/heroes.rb
+++ databases/8.5_challenge/heroes.rb
@@ -12,3 +12,21 @@
   # create a table with map name, player_1, player_2
   # create a method that will allow the user to input information into the table
   # create a method that returns the map with the most wins
+
+  require 'sqlite3'
+
+  # create SQLite3 database
+  db = SQLite3::Database.new("heroes.db")
+  db.results_as_hash = true
+
+  # create a table with map name, player_1, player_2
+  create_table_cmd = <<-SQL
+    CREATE TABLE IF NOT EXISTS heroes_info(
+      id INTEGER PRIMARY KEY,
+      map VARCHAR(255),
+      player_1 VARCHAR(255),
+      player_2 VARCHAR(255)
+    )
+  SQL
+
+  db.execute(create_table_cmd)
