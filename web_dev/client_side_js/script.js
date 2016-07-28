1d0f656445e2322a0f3aaba1d4c46e7b82ab4cea
Long Tran <artsandcrass@gmail.com>
2016-07-28 15:41:17 -0700

    Add code to createa border for h1

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,5 +1,14 @@
 console.log("Hello World");
 
+var title = document.getElementsByTagName('h1');
+title[0].style.border = '3px solid red';
+
+
+
+
+
+
+
 var newParagraph = document.createElement('p');
 var paraText = document.createTextNode('Trying to add this in using our js.');
 newParagraph.appendChild(paraText);
@@ -8,3 +17,5 @@ newParagraph.appendChild(paraText);
 var firstHeading = document.getElementsByTagName('h1');
 var realHeading = firstHeading[0];
 realHeading.appendChild(newParagraph);
+var parent = realHeading.parentNode;
+parent.insertBefore(newParagraph,realHeading);
